1267392f43a375591dd2323c6b956e618fc50a98
Matias Sørensen <sorensen.matias@gmail.com>
2025-09-27 20:56:45 +0200

    Document ScopedResolver

--- Scoper/ScopedResolver.cs
+++ Scoper/ScopedResolver.cs
@@ -5,6 +5,12 @@ namespace Scoper;
 public class ScopedResolver<T> 
     where T : notnull
 {
+    /// <summary>
+    /// Encapsulates a scoped service of type <see cref="T"/> so it can be safely resolved outside a scoped context.
+    /// The service of type <see cref="T"/> should be injected via AddScoped.
+    /// </summary>
+    /// <param name="serviceScope">The scope used to resolve the service</param>
+    /// <returns></returns>
     public T Resolve(IServiceScope serviceScope)
     {
         return serviceScope.ServiceProvider.GetRequiredService<T>();
